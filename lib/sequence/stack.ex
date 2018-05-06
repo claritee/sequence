b9db737df539877394b95d41d30e1b62420ebbbb
Claire Tran <c.tran83@gmail.com>
2018-05-06 20:17:47 +1000

    refactoring to use pattern matching

--- lib/sequence/stack.ex
+++ lib/sequence/stack.ex
@@ -6,8 +6,9 @@ defmodule Sequence.Stack do
   end
 
   def handle_call(:pop, _from, current_stack) do
-    popped = List.first(current_stack)
-    {:reply, popped, List.delete(current_stack, popped)}
+    # popped = List.first(current_stack)
+    [popped | stack] = current_stack
+    {:reply, popped, stack}
   end
 
   def handle_cast({:push, item}, current_stack) do
